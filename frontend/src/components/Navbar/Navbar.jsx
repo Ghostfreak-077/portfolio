e5a8411d5fbc06473e0c2ecef969b5751d3c6008
Partha Pratim Deka <parthapratimdeka129@gmail.com>
2025-08-19 22:38:11 +0530

    fix: nav buttons

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,31 +1,39 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import style from "./navbar.module.css";
 
 const Navbar = ({nav, setNav}) => {
-  const aboutHeight = document.getElementById('about')?.scrollTop || 0;
-  const workHeight = document.getElementById('experience')?.offsetHeight || 0;
-  const projectsHeight = document.getElementById('projects')?.offsetHeight || 0
+
+  const [aboutHeight, setAboutHeight] = useState(null);
+  const [workHeight, setWorkHeight] = useState(null);
+  const [projectsHeight, setProjectsHeight] = useState(null);
+  const [scrolled, setScrolled] = useState(0);
 
   useEffect(() => {
-    const aboutHeight = document.getElementById('about').scrollTop;
-    const workHeight = document.getElementById('experience').scrollTop;
-    const projectsHeight = document.getElementById('projects').scrollTop;
+    setAboutHeight(document.getElementById('about'))
+    setWorkHeight(document.getElementById('experience'))
+    setProjectsHeight(document.getElementById('projects'))
+    
+    window.addEventListener('scroll', () => {
+      setScrolled(window.scrollY);
+    });
   }, [])
   
 
-  const scrolling = (ht)=>{
-    window.scrollTo(0, ht)
+  const scrolling = (ele)=>{
+    ele.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    console.log(ele.offsetTop);
+    
+    setNav(ele.offsetTop);
   }
 
   return (
     <b>
-      <div className={style.nav}>
+      <div className={style.nav} id="navbar">
         <ul className={style.menu}>
-          <li className={nav >= screen.height && nav <= screen.height*2 ?style.select:''} onClick={()=>{scrolling(aboutHeight)}} >About</li>
+          <li className={nav >= screen.height && nav <= screen.height*2 ?style.select:''} onClick={(e)=>{scrolling(aboutHeight)}} >About</li>
           <li className={nav >= screen.height*2 && nav <= screen.height*3 ?style.select:''} onClick={()=>{scrolling(workHeight)}} >Experience</li>
           <li className={nav >= screen.height*3 && nav <= screen.height*4 ?style.select:''} onClick={()=>{scrolling(projectsHeight)}}>Projects</li>
-          {/* <li className={nav >= screen.height*4 && nav <= screen.height*5 ?style.select:''} onClick={()=>{scrolling(4)}}>Publications</li> */}
         </ul>
       </div>
     </b>
